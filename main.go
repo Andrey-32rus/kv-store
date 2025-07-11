910faea7952420fc1d034c7633a4be06ba853475
Simutin Andrey <simutin.andrey2@wb.ru>
2025-07-11 11:01:53 +0300

    refactor

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ import (
 	"github.com/tarantool/go-tarantool/v2"
 )
 
-var conn *tarantool.Connection
+type Service struct {
+	c *tarantool.Connection
+}
 
 func main() {
 	ctx := context.Background()
@@ -29,13 +31,17 @@ func main() {
 		Timeout: 5 * time.Second,
 	}
 
-	var err error
-	conn, err = tarantool.Connect(ctx, dialer, opts)
+	conn, err := tarantool.Connect(ctx, dialer, opts)
 	if err != nil {
 		log.Fatalf("Cannot connect to Tarantool: %s", err)
 	}
+	defer conn.Close()
 
-	http.HandleFunc("/kv/", kvHandler)
+	s := &Service{
+		c: conn,
+	}
+
+	http.HandleFunc("/kv/", s.kvHandler)
 
 	fmt.Println("Server listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -43,7 +49,8 @@ func main() {
 	}
 }
 
-func kvHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Service) kvHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	key := r.URL.Path[len("/kv/"):]
 
 	switch r.Method {
@@ -53,63 +60,73 @@ func kvHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Cannot read body", http.StatusBadRequest)
 			return
 		}
-
-		f := conn.Do(tarantool.NewReplaceRequest("kv").
-			Context(r.Context()).
-			Tuple([]interface{}{key, string(body)}))
-		<-f.WaitChan()
-		_, err = f.Get()
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Tarantool error: %s", err), http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
+		s.PutHandler(ctx, key, body, w)
 
 	case http.MethodGet:
-		req := tarantool.NewSelectRequest("kv").
-			Context(r.Context()).
-			Index("primary").
-			Iterator(tarantool.IterEq).
-			Limit(1).
-			Key([]interface{}{key})
-		f := conn.Do(req)
-		<-f.WaitChan()
-		v, err := f.Get()
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Tarantool error: %s", err), http.StatusInternalServerError)
-			return
-		}
-		if len(v) == 0 {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		value, ok := v[0].([]interface{})[1].(string)
-		if !ok {
-			http.Error(w, "Unexpected tuple format", http.StatusInternalServerError)
-			return
-		}
-		_, err = w.Write([]byte(value))
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Write error: %s", err), http.StatusInternalServerError)
-		}
+		s.GetHandler(ctx, key, w)
 
 	case http.MethodDelete:
-		f := conn.Do(
-			tarantool.NewDeleteRequest("kv").
-				Context(r.Context()).
-				Index("primary").
-				Key([]interface{}{key}),
-		)
-		<-f.WaitChan()
-		_, err := f.Get()
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Tarantool error: %s", err), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusNoContent)
-
+		s.DeleteHandler(ctx, key, w)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+}
+
+func (s *Service) PutHandler(ctx context.Context, key string, body []byte, w http.ResponseWriter) {
+	f := s.c.Do(tarantool.NewReplaceRequest("kv").
+		Context(ctx).
+		Tuple([]interface{}{key, string(body)}))
+	<-f.WaitChan()
+	_, err := f.Get()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Tarantool error: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (s *Service) GetHandler(ctx context.Context, key string, w http.ResponseWriter) {
+	req := tarantool.NewSelectRequest("kv").
+		Context(ctx).
+		Index("primary").
+		Iterator(tarantool.IterEq).
+		Limit(1).
+		Key([]interface{}{key})
+	f := s.c.Do(req)
+	<-f.WaitChan()
+	v, err := f.Get()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Tarantool error: %s", err), http.StatusInternalServerError)
+		return
+	}
+	if len(v) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	value, ok := v[0].([]interface{})[1].(string)
+	if !ok {
+		http.Error(w, "Unexpected tuple format", http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write([]byte(value))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Write error: %s", err), http.StatusInternalServerError)
+	}
+}
+
+func (s *Service) DeleteHandler(ctx context.Context, key string, w http.ResponseWriter) {
+	f := s.c.Do(
+		tarantool.NewDeleteRequest("kv").
+			Context(ctx).
+			Index("primary").
+			Key([]interface{}{key}),
+	)
+	<-f.WaitChan()
+	_, err := f.Get()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Tarantool error: %s", err), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
